2c92fc056da032c05cc7f0dd97d15fcb829cf647
Stephanie Lin <slin.animation@gmail.com>
2025-12-17 15:59:14 -0500

    wave 4 in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,59 @@
 import TaskList from './components/TaskList.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 const toggleCompleteTask = task => {
-  console.log(`${task.title} has been completed!`)
+  console.log(`${task.title} has been completed!`);
   return { ...task, isComplete: !task.isComplete };
 };
 
+const kbaseURL = 'http://127.0.0.1:5000';
+
+const getAllTasksAPI = () => {
+  return axios.get(`${kbaseURL}/tasks`)
+    .then(response => response.data)
+    .catch(error => console.log(error));
+};
+
+const convertFromAPI = (task) => {
+  const { id, title, description } = task;
+  const newTask = {
+    id,
+    title,
+    description,
+    isComplete: task.is_complete
+  };
+  return newTask;
+};
+
+
+
 const App = () => {
-  const [taskData, setTaskData] = useState(TASKS);
+  const [taskData, setTaskData] = useState([]);
+
+  const getAllTasks = () => {
+    return getAllTasksAPI()
+      .then(tasks => {
+        setTaskData(tasks);
+      });
+  };
+
+  useEffect(() => {
+    getAllTasks();
+  }, []);
 
   const handleComplete = (id) => {
     setTaskData((prevTaskData) => {
